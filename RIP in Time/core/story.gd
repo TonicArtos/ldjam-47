7c3e2019c2e71fffb1d87c06b80f36600fe43467
Ana Rakonjac <wavicle@gmail.com>
2020-10-04 22:33:42 +0800

    Update story.gd
    Filled in blurbs.

--- RIP in Time/core/story.gd
+++ RIP in Time/core/story.gd
@@ -9,11 +9,160 @@ const text = {
 	"look at room1": "room1 description",
 	
 	"look at battery": """Mini-Fuse 5778K
-“The power of the Sun in the palm of your hand.”""",
+	“The power of the Sun in the palm of your hand.”""",
 
 	"operate rip drive": "You start the rip sequence to head back to civilisation and the RIP Drive thrums to life, but something seems to be wrong. The radio signal appears to have modified its programming. A tiny rip in space tears open before you as you hear the ship's reactor containment alarm begin to sound.",
 	
 	"look at ripdrive": "The R.I.P. Drive™."
+	
+	"anfelice gray journal": """ANFELICE GRAY
+	JOURNAL ENTRY
+	Date: 14:14:59 17 September 3030 EET
+
+	Last time we went to sleep, I had the worst nightmare. A tiny black hole blasted through the ship and sucked us in one by one. Even though we’re meant to be near-dead in the cryo-pods, I felt the terror in my bones. It made me dread this sleep. I hope we don’t have to do many more jumps after this, because I don’t think I can take it.""",
+
+	"whetu ng journal": """WHETU NG
+	JOURNAL ENTRY
+	Date: 13:58:32 17 September 3030 EET
+
+	REMINDER FOR WHEN YOU’RE AWAKE! Steve owes you 20 ripcoins. Don’t let him scam you this time.""",
+
+	"steve lowry journal": """STEVE LOWRY
+	JOURNAL ENTRY
+	Date: 18:06:56 17 September 3030 EET
+
+	Robbie’s getting smarter. The other day, I told her that the eliminator in cubicle 6 was busted. When she went to inspect it, I put a cucumber behind her so she’d see it and freak out when she turned around. I thought she was just checking out the plumbing, so I waited a bit, but then she asked me to get her a plunger. I turned around and tripped right over her damn arm! I don’t know how she got it there without me noticing, but boy did it hurt when I hit the floor. We should turn the gravity down in this place."""
+
+	"jonie hoff journal": """JONIE HOFF
+	JOURNAL ENTRY
+	Date: 11:12:13 17 September 3030 EET
+
+	I’m tired of the finance industry. We’re out here doing the hard labour while the fat cats are windsurfing on Europa or whatever it is they do all day. When my tour is over, my friends will all be retired and kicking back with their grandkids, and I’m going to be in my thirties. Don’t even get me started on the nightmares…."""
+
+	"reddy koppel journal": """REDDY KOPPEL
+	JOURNAL ENTRY
+	Date: 17:10:43 17 September 3030 EET
+
+	Things to do after the RIP jump:
+	- Book hotel on Luna for shore leave ASAP. Do not forget!!!
+	- Make appointment with psych. Bad dreams probably about the divorce.
+	- Clean the fridge."""
+
+	"alisha doil journal": """ALISHA DOIL
+	JOURNAL ENTRY
+	Date: 15:01:44 17 September 3030 EET
+
+	I’ve been thinking about it, and there’s probably a more efficient way to mine ripcoins if the RIP drive is close to a binary black hole system, especially if the black holes have a rapid orbit around one another. Gravitational forces might be a problem. Will look into this later."""
+
+	"common room door status": """EMERGENCY LOCK ACTIVATED
+	CONDITION 8: SUDDEN LOSS OF CABIN PRESSURE"""
+
+	"bridge door status": """EMERGENCY LOCK ACTIVATED
+	CONDITION 8: SUDDEN LOSS OF CABIN PRESSURE"""
+
+	"reactor room door status": """EMERGENCY LOCK ACTIVATED
+	CONDITION 8: SUDDEN LOSS OF CABIN PRESSURE"""
+
+	"fire axe cabinet message": """In case of emergency, break glass."""
+	
+	"fire axe description": """Wallmutt Fire Axe - Real™ oak handle
+	Caution: flammable"""
+
+	"nav computer message": """“Hi there! Tell me where you want to go!”"""
+
+	"main computer message": """User ID: 
+	Password: """
+
+	"instance processor message": """-- sleep mode --"""
+
+	"postit note text": """ID: admin
+	PW: ripcoinzzz1!"""
+
+	"gun description": """SAG-9000
+	“We put the AI in AIM!”"""
+
+	"main commputer login success": """Login successful! What would you like to do?"""
+	
+	"main computer options": """1. View ship log
+	2. Initiate self-destruct sequence
+	3. Access security fob"""
+	
+	"main comp opt 1": """We are prepped and ready for the next RIP jump. I have warned the crew that based on the incidence pattern so far, we are likely to encounter void interference. However, this was dismissed by Captain Koppel. Should anything go wrong, I will quote him for posterity: “Eh, no big deal. You bots are always so paranoid. Just chill out and enjoy the ride.”"""
+
+	"main comp opt 2": """Are you sure?"""
+	
+	"main comp opt 3": """Access security fob"""
+	
+	"fob message": """Don’t lose me! There are no more spare copies!"""
+
+	"ripdrive menu": """Welcome to the Recursive Instance Projector Drive! What would you like to do?
+	
+	1. Send a message
+	2. Send an object"""
+	
+	"rip comp opt 1": """Please type your message:"""
+	
+	"rip comp opt 2": """Send an object"""
+
+	"reactor terminale message": """Access to reactor terminal granted! What would you like to do?
+	Power up the reactor -> already on! Cannot.
+	Power down the reactor -> reactor powers down"""
+	
+	"incident report good": """INCIDENT REPORT
+	CATEGORY: NEAR MISS
+	According to records, this is the third time a void contaminant has been found in the reactor after a RIP jump. Thankfully, there was no physical damage to the ship or its occupants. What is causing these incidents? Is the Health and Safety team even reading these reports?"""
+	
+	"incident report bad": """INCIDENT REPORT
+	CATEGORY: PSYCHOLOGICAL INJURY
+
+	According to records, this is the third time a void contaminant has been found in the reactor after a RIP jump. Thankfully, there was no physical damage to the ship or its occupants, though several human crew members have developed severe void dementia. They were put into cryo-sleep and delivered to health care facilities at the recent planetside rendezvous. What is causing these incidents? Is the Health and Safety team even reading these reports?"""
+
+	"robot dead end": """-- memory chip not found --
+	-- qubit core not found --
+	-- superdeep logic module not found --
+	-- error --
+	-- error --
+	-- error --"""
+	
+	"anfelice gray good end": """“Gosh, that was awful. I’m going to apply for a transfer. NO space travel and NO cryo-sleep, thank you very much!”"""
+	
+	"anfelice gray bad end": """“There’s nothing. There’s nothing. There’s nothing.”"""
+	
+	"whetu ng good end": """“Steve, you better give me those 20 ripcoins, or I’m flushing your bottle cap collection out of the airlock!”"""
+	
+	"whetu ng bad end": """“My name? I can’t remember.”"""
+	
+	"steve lowry good end": """“I told Whetu that I was investing his money. I can’t just uninvest it! Anyway, he now owns 1/32 of an ice farm in the belt. When the ice matures, we’ll harvest and sell it off. I don’t know why he’s so upset. This is nothing like my vacuum bottling business.”"""
+
+	"steve lowry bad end": """“The cucumber-- It’s after us!”"""
+	
+	"jonie hoff good end": """“Forget what I said about quitting finance. The latest ripcoin haul was MASSIVE. I’m buying a yacht and one of those cute robotic poodles with Daisy’s memories built into it. Then I’m going into solo consulting. Five year plan, here I come!”"""
+
+	*jonie off bad end": """“Have you seen the endless black abyss?”"""
+	
+	"reddy koppel good end": """“If we stop for maintenance now, that’s two RIP jumps lost. NO ONE is going to be happy about that, not the higher ups and not the crew. It’s been okay so far, so the ship will hang on a little longer.”"""
+	
+	"reddy kopel bad end": """“Alone in space, alone in space.”"""
+	
+	"alishia doil good end": ***“There’s something wrong. Too many people are having strange experiences while in cryo. Maybe the chambers are malfunctioning? I guess the medic techs will handle it. I need to get back to working on the black hole idea.”"""
+
+	"alisha doil bad end": “Don’t bother me. The void calls.”
+	
+	
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
 }
 
 static func default_done() -> Option:
